182282f38f361591b0141d4c181221d6998100f7
Humberto Della Torre <humbertodlltrr@gmail.com>
2017-05-16 15:31:31 -0600

    Update exercises1.py

--- exercises1.py
+++ exercises1.py
@@ -2,5 +2,5 @@
 Author: Humberto Della Torre
 
 Several python exercises to practice programming
-Exercises obtained from https://github.com/zhiwehu/Python-programming-exercises
+Exercises obtained from http://codingbat.com/python
 """

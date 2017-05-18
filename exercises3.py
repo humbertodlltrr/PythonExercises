2a3cd49888dec49db30b12a5658ebe9d053b2ac5
Humberto Della Torre <humbertodlltrr@gmail.com>
2017-05-18 15:56:07 -0600

    Update exercises3.py

--- exercises3.py
+++ exercises3.py
@@ -1,1 +1,25 @@
+#Character Input
+import datetime
+def character_input():
+    name = input("What's your name?")
+    age = int(input("How old are you?"))
+    num = int(input("Favorite number?"))
+    year = str(datetime.datetime.now().year - age + 100)
+    print(num*("Hey "+name+"! You will turn 100 in "+year+"\n"))
 
+#Odd Or Even
+def odd_even():
+    num = int(input("Choose a number:"))
+    if num%4 == 0:
+        print("Multiple of 4!")
+    elif num%2 == 0:
+        print("It's even!")
+    else:
+        print("It's odd!")
+    check = int(input("Another number:"))
+    if(num%check == 0):
+        print("Divides evenly")
+    else:
+        print("Doesn't divide evenly")
+
+#List Less Than 10
